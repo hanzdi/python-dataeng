99a04c0efcee81e6332dcf95f11d75cfedb3a6d9
chrispi21 <70602953+chrispi21@users.noreply.github.com>
2025-03-11 08:04:21 +0000

    04_main - bonus

---
+++ 04_main.py
@@ -0,0 +1,20 @@
+import sys
+import pandas as pd
+
+def extract(path):
+  return pd.read_csv(path)
+
+def transform(df):
+  return df[["Imię"]].drop_duplicates()
+
+def load(df, path):
+  return df.to_json(path, orient="records", index=False, lines=True)
+
+
+def job(input_path, output_path):
+  source_data = extract(input_path)
+  transformed_data = transform(source_data)
+  load(transformed_data, output_path)
+
+if __name__ == '__main__':
+    job(sys.argv[1], sys.argv[2])
