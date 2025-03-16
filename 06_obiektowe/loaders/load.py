a7e84008efe35e89165f3b17d932609a354e41ae
hanzdi <121463935+hanzdi@users.noreply.github.com>
2025-03-16 15:26:19 +0000

    Refactoring 06 - v1

---
+++ 06_obiektowe/loaders/load.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class jsonLoader:
+  def __init__(self,orient, index, lines):
+    self.orient = orient
+    self.index = index
+    self.lines = lines
+  def load(self, df, path):
+    return df.to_json(path, orient=self.orient, index=self.index, lines=self.lines)
+
+
